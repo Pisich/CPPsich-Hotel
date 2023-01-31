0daf403b980ebb0e06279ad75f357fa3b61dc853
Pisich <croc1110@hotmail.com>
2023-01-31 10:12:07 -0600

    Added Room class constructor

--- main.cpp
+++ main.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
 #include <string>
 
-enum class RoomState{
+using namespace std;
+
+enum RoomState
+{
   Available,
   Cleaning,
   Remodeling,
   Occupied,
   OutOfService
-}
+};
 
-class Room{
-  protected:
-    size_t floor;
-    char roomSection;
-    RoomState roomState;
-    double costPerNight;
-    size_t totalOccupancy;
-    size_t actualOccupancy;
+class Room
+{
+protected:
+  size_t floor;
+  char section;
+  RoomState roomState;
+  double costPerNight;
+  size_t totalOccupancy;
+  size_t actualOccupancy;
+
+public:
+  const size_t roomNumber;
+  string description;
+
+  Room(size_t roomNumber, string description, size_t floor, char section,
+       RoomState roomState, double costPerNight, size_t totalOccupancy, size_t actualOccupancy) :
+          roomNumber(roomNumber), description(description), floor(floor), section(section),
+          roomState(roomState), costPerNight(costPerNight), totalOccupancy(totalOccupancy),
+          actualOccupancy(actualOccupancy) {}
   
-  public:
-    size_t roomNumber;
-    string roomDescription;
+  size_t getRoomFloor(){
+    return floor;
+  }
+  char getRoomSection(){
+    return section;
+  }
+  RoomState getRoomState(){
+    return roomState;
+  }
+  double getCostPerNight(){
+    return costPerNight;
+  }
+  size_t getTotalOccupancy(){
+    return totalOccupancy;
+  }
+  size_t getActualOccupancy(){
+    return actualOccupancy;
+  }
+};
 
-}
-
-int main() {
+int main()
+{
+  Room x{255, "Showcase room", 2, 'C', RoomState::Available, 45.5, 12, 6};
   std::cout << "Hello World!";
   return 0;
 }
