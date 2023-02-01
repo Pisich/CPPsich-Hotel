aae40f7a13f52176e66037e77489a0118be0d0d8
Pisich <croc1110@hotmail.com>
2023-02-01 10:41:56 -0600

    Created Room class setters

--- main.cpp
+++ main.cpp
@@ -50,11 +50,40 @@ public:
   size_t getActualOccupancy(){
     return actualOccupancy;
   }
+
+  int setRoomFloor(size_t newVal){
+    this->floor = newVal;
+    return 0;
+  }
+  int setRoomSection(char newVal){
+    this->section = newVal;
+    return 0;
+  }
+  int setRoomState(RoomState newVal) noexcept {
+    this->roomState = newVal;
+    return 0;
+  }
+  int setCostPerNight(double newVal) noexcept {
+    if (newVal < 0) return 1;
+    this->costPerNight = newVal;
+    return 0;
+  }
+  int setTotalOccupancy(size_t newVal) noexcept {
+    this->totalOccupancy = newVal;
+    return 0;
+  }
+  int setActualOccupancy(size_t newVal) noexcept {
+    if (newVal < 0) return 1;
+    if (newVal == 0) setRoomState(RoomState::Cleaning);
+
+    this->actualOccupancy = newVal;
+    return 0;
+  }
 };
 
 int main()
 {
   Room x{255, "Showcase room", 2, 'C', RoomState::Available, 45.5, 12, 6};
-  std::cout << "Hello World!";
+  std::cout << x.description;
   return 0;
 }
